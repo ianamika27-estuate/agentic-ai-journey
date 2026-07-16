a4df12f8a4c58ef3d55852453ed2f97d635b6d02
ianamika27-estuate <anamika.kumari@estuate.com>
2026-07-16 12:58:10 -0700

    attempt 1: failures 0 -> 0

--- tasks/buggy.py
+++ tasks/buggy.py
@@ -1,13 +1,18 @@
 def add_numbers(a, b):
     """Should return the sum of a and b."""
-    return a - b  # bug: should be a + b
+    return a + b  # fixed: was a - b
 
 
 def reverse_string(s):
     """Should return the string reversed."""
-    return s  # bug: doesn't actually reverse
+    return s[::-1]  # fixed: now actually reverses the string
 
 
 def is_even(n):
     """Should return True if n is even, False otherwise."""
-    return n % 2 == 1  # bug: logic is inverted
+    return n % 2 == 0  # fixed: was == 1 (inverted logic)
+
+
+def subtract_numbers(a, b):
+    """Should return the difference of a and b."""
+    return a - b
